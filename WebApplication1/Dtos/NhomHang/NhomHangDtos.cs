c7899b44de38fb734d77b8ec159396d06550707a
PinoNg3uyen <nguyenmanhcuong.bh1234@gmail.com>
2025-10-25 16:13:25 +0700

    bai_moi_nhat_10_25
    10_25

---
+++ WebApplication1/Dtos/NhomHang/NhomHangDtos.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Dtos.NhomHang
+{
+    public class NhomHangDtos : Controller
+    {
+        public int NhomId { get; set; }
+        public string TenNhom { get; set; } = "";
+
+        public string GhiChu { get; set; } = string.Empty;
+
+        internal object TolistAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
